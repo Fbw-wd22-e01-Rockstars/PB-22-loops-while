2d5eee71d6f4ab912bdca6b2f88c1e50e8d2e2e7
nrcool <nomanalam7214@gmail.com>
2022-05-03 11:05:07 +0200

    solution

---
+++ index.js
@@ -0,0 +1,34 @@
+// ## 1. Run Along
+
+
+// Create a `while` loop that runs as long as the variable `i` is less than 15. Print `i`.
+let i = 0;
+while(i<15){
+    console.log(i)
+    i++;
+}
+
+// ## 2. Add it up 
+// Create a `while` loop which sums up numbers from 1-20.
+let x = 1;
+let sum = 0;
+
+while(x<=20){
+    sum += x
+    //sum=sum+x
+    x++;
+}
+console.log(sum)
+
+// ## 3. Do this while i...
+// Use a `do`, `while` loop to print `The number is [i]` while i is less than 20. 
+
+//while()
+let y=0;
+
+do{
+    console.log(`The number is [${y}]`)
+    y++;
+    
+}
+while(y<20);
